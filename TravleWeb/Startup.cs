ff8c3f1f18644612dcf5e16ab93c43c74a14460b
letrunghieu <dendivn322@Gmail.com>
2017-10-31 20:13:43 +0700

    Add project files.

---
+++ TravleWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TravleWeb.Startup))]
+namespace TravleWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
